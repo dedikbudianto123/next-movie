b5e78cfa63276a541fc0b577aa03454af37f8f09
irfan andriansyah <irfanandriansyah10@gmail.com>
2021-01-24 16:10:27 +0700

    fix(ci): implement stylelint in webpack config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
+/* eslint-disable import/no-extraneous-dependencies */
 const path = require('path');
+const StylelintPlugin = require('stylelint-webpack-plugin');
 
 module.exports = {
   sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
+    includePaths: [path.join(__dirname, 'styles')]
   },
+  webpack: (config, { webpack }) => {
+    // Note: we provide webpack above so you should not `require` it
+    config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//));
+    config.plugins.push(new StylelintPlugin());
+
+    return config;
+  }
 };
